da2365fe9dc6e62172eed623e0333159c57c6a23
Codezey <91379167+C0DEZEY@users.noreply.github.com>
2023-02-06 17:05:07 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,16 +3,16 @@ module.exports = {
   Prefix: "-", 
 
   Users: {
-    OWNERS: ["333616865781743616", "885716877219860522"] 
+    OWNERS: ["", ""] 
   },
 
   Handlers: {
-    MONGO: "mongodb+srv://Codezey:8h4fUr9R717ed7Ts@cavernbot.eywocuu.mongodb.net/?retryWrites=true&w=majority" // MONGO URI // Mongo password 8h4fUr9R717ed7Ts
+    MONGO: "" 
   },
 
   Client: {
-    TOKEN: "MTAyMTEwNTA1NzIwMDA5NTMzMw.G-ilV7.7mSu2w0TEkgkdfx3OvG7SXrbi584FnQS6_q6yQ",
-    ID: "1021105057200095333" 
+    TOKEN: "",
+    ID: "" 
   }
 
 }
